30d26db3708e1d8288b4abab6396ea4a5fca0963
Adrian Galewski <netcoderpl@gmail.com>
2010-10-21 10:54:37 +0000

    refs #69 - invitation table merged into participant

--- php/src/application/deploy/database/004 participant.sql
+++ php/src/application/deploy/database/004 participant.sql
@@ -32,10 +32,16 @@ CREATE TABLE IF NOT EXISTS `participant`
     `netBout` MEDIUMINT UNSIGNED NOT NULL COMMENT "Unique ID of the NetBout",
 
     -- MEDIUMINT UNSIGNED is used - @see user.id column
-    `user` MEDIUMINT UNSIGNED NOT NULL COMMENT "Invited participant",
+    `invited` MEDIUMINT UNSIGNED NOT NULL COMMENT "Invited participant",
+
+    -- MEDIUMINT UNSIGNED is used - @see user.id column
+    `inviter` MEDIUMINT UNSIGNED NOT NULL COMMENT "Who send this invitation",
+
+    -- We using SHA256 hash with, so it's always 64 chars,
+    `secretKey` CHAR(64) NOT NULL COMMENT "Secret key used for invitation identification",
 
     -- Participant status in this NetBout
-    `status` ENUM('creator', 'invited', 'accepted', 'rejected') NOT NULL COMMENT "Participant status",
+    `status` ENUM('creator', 'pending', 'invited', 'accepted', 'rejected') NOT NULL COMMENT "Participant status",
 
     -- Links are identified by ID
     PRIMARY KEY(`id`),
@@ -45,12 +51,18 @@ CREATE TABLE IF NOT EXISTS `participant`
         ON UPDATE CASCADE
         ON DELETE CASCADE,
 
-    -- Link to the user
-    FOREIGN KEY(`user`) REFERENCES `user`(`id`)
+    -- Link to the invited user
+    FOREIGN KEY(`invited`) REFERENCES `user`(`id`)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
 
-    UNIQUE(`netBout`, `user`)
+    -- Link to the invited user
+    FOREIGN KEY(`inviter`) REFERENCES `user`(`id`)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    UNIQUE(`netBout`, `invited`),
+    INDEX(`secretKey`)
 )
 AUTO_INCREMENT=1
 DEFAULT CHARSET=utf8 
--- php/test/Model/NetBout/ParticipantTest.php
+++ php/test/Model/NetBout/ParticipantTest.php
@@ -11,4 +11,62 @@ class Model_NetBout_ParticipantTest extends FaZend_Test_TestCase
     {
         $this->assertTrue(class_exists(substr(get_class(), 0, -4)));
     }
+
+    public function testInvitationCanBeSent()
+    {
+        $inviter = Mocks_Model_User::get(null, 'testuser1');
+        $invited = Mocks_Model_User::get('invited@email.com');
+        $netBout = Mocks_Model_NetBout::get();
+
+        $participant = Model_NetBout_Participant::create($netBout, $invited, $inviter);
+
+        // @todo remove this line when this test will be workable
+        $this->markTestIncomplete();
+
+        $participant->sendInvitation();
+    }
+
+    public function testInvitationCanBeAccepted()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testInvitationCanBeDeclined()
+    {
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * When user accept invitation, it should be able to participate in the
+     * NetBout for which he was invited
+     *
+     * @todo #16:2h Model_Acl should be implemented with persistence storage to make this
+     * scenario possible
+     */
+    public function testUserHasAccetToNetBoutAfterInvitationAcceptance()
+    {
+        $this->markTestIncomplete();
+
+        $inviter = Mocks_Model_User::get(null, 'testuser1');
+        $netBout = Mocks_Model_NetBout::get($inviter);
+        $this->assertTrue(Model_Acl::isAllowed($inviter, $netBout));
+
+        $invited = Mocks_Model_User::get('invited@email.com');
+
+        $this->assertFalse(Model_Acl::isAllowed($user, $netBout, Model_Acl::PRIVILEGE_PARTICIPATE));
+        $participant = Model_NetBout_Participant::create($netBout, $invited, $inviter);
+        $participant->acceptInvitation();
+        $this->assertTrue(Model_Acl::isAllowed($user, $netBout, Model_Acl::PRIVILEGE_PARTICIPATE));
+    }
+
+    /**
+     * Expired invitations
+     *
+     * Not accepted invitation should be automatically declined after some
+     * configured interval of time (7 days)
+     */
+    public function testInvitationAreAutomaticallyDeclinedAfterExpirationInterval()
+    {
+       $this->markTestIncomplete();
+    }
 }
