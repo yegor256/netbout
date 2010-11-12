74baed2fbeb0962f570165daf2baf983741af0df
Yegor Bugayenko <yegor@tpc2.com>
2010-11-12 12:24:28 +0000

    typo

--- php/src/application/controllers/UserController.php
+++ php/src/application/controllers/UserController.php
@@ -2,12 +2,12 @@
 /**
  * netbout.com
  *
- * Redistribution and use in source and binary forms, with or 
- * without modification, are PROHIBITED without prior written 
- * permission from the author. This product may NOT be used 
- * anywhere and on any computer except the server platform of 
- * netbout.com. located at www.netbout.com. If you received this 
- * code occasionally and without intent to use it, please report 
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are PROHIBITED without prior written
+ * permission from the author. This product may NOT be used
+ * anywhere and on any computer except the server platform of
+ * netbout.com. located at www.netbout.com. If you received this
+ * code occasionally and without intent to use it, please report
  * this incident to the author by email: privacy@netbout.com
  *
  * @author Yegor Bugayenko <yegor256@yahoo.com>
@@ -26,28 +26,28 @@ require_once 'FaZend/Controller/Action.php';
  *
  * @package front
  */
-class UserController extends Fazend_UserController
+class UserController extends FaZend_Controller_Action
 {
-    
+
     /**
      * Login
      *
      * @return void
      * @see views/scripts/user/login.phtml
      */
-    public function loginAction() 
+    public function loginAction()
     {
     }
-    
+
     /**
      * Account management
      *
      * @return void
      * @see views/scripts/user/account.phtml
      */
-    public function accountAction() 
+    public function accountAction()
     {
-        
+
     }
 
 }
