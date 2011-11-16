26b6458b7e1a4926f5c593d53a86a9117dccee6e
Yegor Bugayenko <yegor@tpc2.com>
2011-11-16 11:42:43 +0000

    refs #117 - polished db config

--- netbout/netbout-db/src/main/java/com/netbout/db/Database.java
+++ netbout/netbout-db/src/main/java/com/netbout/db/Database.java
@@ -141,7 +141,7 @@ public final class Database {
                 new ClassLoaderResourceAccessor(),
                 new JdbcConnection(Database.source.getConnection())
             );
-            liquibase.update(2, "netbout");
+            liquibase.update("netbout");
         } catch (liquibase.exception.LiquibaseException ex) {
             throw new IllegalStateException(ex);
         } catch (SQLException ex) {
