916f3c38962d856c569837788a5a7d6a1f47cb9b
Yegor Bugayenko <yegor@tpc2.com>
2012-01-15 15:12:02 +0000

    refs #171 - Bumper introduced and tested, works fine

---
+++ netbout/netbout-rest/src/mock/java/com/netbout/rest/bumper/BumperRsMocker.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2009-2011, netBout.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are PROHIBITED without prior written permission from
+ * the author. This product may NOT be used anywhere and on any computer
+ * except the server platform of netBout Inc. located at www.netbout.com.
+ * Federal copyright law prohibits unauthorized reproduction by any means
+ * and imposes fines up to $25,000 for violation. If you received
+ * this code occasionally and without intent to use it, please report this
+ * incident to the author by email.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+package com.netbout.rest.bumper;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * Bumper front page.
+ * @author Yegor Bugayenko (yegor@netbout.com)
+ * @version $Id$
+ */
+@Path("/bumper")
+public final class BumperRsMocker {
+
+    /**
+     * Set URI Info.
+     * @param info The info to inject
+     */
+    @Context
+    public void setUriInfo(final UriInfo info) {
+        BumperFarmMocker.setHome(info.getBaseUri());
+    }
+
+    /**
+     * Namespace page.
+     * @return The content
+     */
+    @GET
+    @Path("/ns")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String namespace() {
+        return "try /bumper/ns.xsd instead";
+    }
+
+    /**
+     * XSD page.
+     * @return The XSD
+     */
+    @GET
+    @Path("/ns.xsd")
+    @Produces(MediaType.TEXT_XML)
+    public String xsd() {
+        // @checkstyle StringLiteralsConcatenation (3 lines)
+        return "<?xml version='1.0'?><xs:schema"
+            + " xmlns:xs='http://www.w3.org/2001/XMLSchema'"
+            + " xmlns:b='/bumper/ns'"
+            + " targetNamespace='/bumper/ns'>"
+            + "<xs:element name='bump' type='xs:string'/>"
+            + "</xs:schema>";
+    }
+
+}
---
+++ netbout/netbout-rest/src/test/rexsl/scripts/bumper/PostsXmlMessages.groovy
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2009-2011, netBout.com
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are PROHIBITED without prior written permission from
+ * the author. This product may NOT be used anywhere and on any computer
+ * except the server platform of netBout Inc. located at www.netbout.com.
+ * Federal copyright law prohibits unauthorized reproduction by any means
+ * and imposes fines up to $25,000 for violation. If you received
+ * this code occasionally and without intent to use it, please report this
+ * incident to the author by email.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * @author Yegor Bugayenko (yegor@netbout.com)
+ * @version $Id$
+ */
+package com.netbout.rest.rexsl.bumper
+
+import com.netbout.spi.Urn
+import com.netbout.spi.client.RestSession
+import javax.ws.rs.core.UriBuilder;
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+
+def paul = new RestSession(rexsl.home).authenticate(new Urn('urn:test:paul'), '')
+def xsd = UriBuilder.fromUri(rexsl.home).path('/bumper/ns.xsd').build()
+
+def bout = paul.start()
+bout.rename('Posting XML messages to bumper')
+bout.invite(paul.friend(new Urn('urn:test:bumper')))
+bout.post("""<?xml version='1.0'?>
+    <bump xmlns='/bumper/ns'
+        xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+        xsi:schemaLocation='/bumper/ns ${xsd}' />
+""")
