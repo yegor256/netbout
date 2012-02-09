156ac6362918f283990d97679b5f1c728927fea8
Yegor Bugayenko <yegor@tpc2.com>
2012-02-09 04:16:13 +0000

    refs #249 - extra stopper

--- netbout/netbout-rest/src/main/java/com/netbout/notifiers/email/EmailMessage.java
+++ netbout/netbout-rest/src/main/java/com/netbout/notifiers/email/EmailMessage.java
@@ -66,6 +66,7 @@ final class EmailMessage {
             "Sent via Netbout: https?://.*",
             "-+original\\s+message-+",
             ">.*",
+            "\u8212",
             // @checkstyle LineLength (1 line)
             "On \\w{3}, \\w{3} \\d{1,2}, \\d{4} at \\d{2}:\\d{2} (AM|PM),.*",
         };
