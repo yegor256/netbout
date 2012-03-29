2eb367f4a913de38b0bf7902917b551baf87dc6f
Yegor Bugayenko <yegor@tpc2.com>
2012-03-29 17:34:27 +0000

    refs #338 - less logs in SPI

--- spi/src/main/java/com/netbout/spi/cpa/CpaHelper.java
+++ spi/src/main/java/com/netbout/spi/cpa/CpaHelper.java
@@ -125,7 +125,7 @@ public final class CpaHelper implements Helper {
         final long start = System.nanoTime();
         final ConcurrentMap<String, HelpTarget> found =
             new OpDiscoverer(this.identity).discover(url);
-        Logger.info(
+        Logger.debug(
             this,
             "#init('%s'): %d operations discovered in %[nano]s: %[list]s",
             url,
