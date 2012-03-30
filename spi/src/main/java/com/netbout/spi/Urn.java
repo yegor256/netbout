5614cf3105853d82f8f1ba4d5d75fec293a04cb5
Yegor Bugayenko <yegor@tpc2.com>
2012-03-30 01:58:01 +0000

    refs #339 - URN format validation fixed

--- spi/src/main/java/com/netbout/spi/Urn.java
+++ spi/src/main/java/com/netbout/spi/Urn.java
@@ -73,7 +73,7 @@ public final class Urn implements Comparable, Serializable {
      */
     private static final String REGEX =
         // @checkstyle LineLength (1 line)
-        "^urn:[a-z]{1,31}(:([\\-a-zA-Z0-9/]|%[0-9a-fA-F]{2})*)+(\\?[a-z0-9]+(=([\\-a-zA-Z0-9/]|%[0-9a-fA-F]{2})*)?(&[a-z0-9]+(=([\\-a-zA-Z0-9/]|%[0-9a-fA-F]{2})*)?)*)?\\*?$";
+        "^urn:[a-z]{1,31}(:([\\-a-zA-Z0-9/]|%[0-9a-fA-F]{2})*)+(\\?\\w+(=([\\-a-zA-Z0-9/]|%[0-9a-fA-F]{2})*)?(&\\w+(=([\\-a-zA-Z0-9/]|%[0-9a-fA-F]{2})*)?)*)?\\*?$";
 
     /**
      * The URI.
