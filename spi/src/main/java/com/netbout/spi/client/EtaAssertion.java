4065062b5c4d8ece1cf1d72efaf2205231275a1b
Yegor Bugayenko <yegor@tpc2.com>
2012-08-30 14:57:58 +0000

    refs #458 - quiet assertions

--- spi/src/main/java/com/netbout/spi/client/EtaAssertion.java
+++ spi/src/main/java/com/netbout/spi/client/EtaAssertion.java
@@ -41,6 +41,7 @@ import java.util.concurrent.TimeUnit;
  * @author Yegor Bugayenko (yegor@netbout.com)
  * @version $Id$
  */
+@AssertionPolicy.Quiet
 public final class EtaAssertion implements AssertionPolicy {
 
     /**
@@ -69,22 +70,17 @@ public final class EtaAssertion implements AssertionPolicy {
                 response.xpath("/page/identity/eta/text()").get(0)
             );
             if (this.eta > 0) {
-                Logger.warn(
-                    this,
-                    // @checkstyle LineLength (1 line)
-                    "assertThat(..): ETA=%[nano]s reported for '%s', the page is not ready",
-                    this.eta,
-                    response.xpath("/page/identity/name/text()").get(0)
+                throw new AssertionError(
+                    Logger.format(
+                        "ETA=%[nano]s for '%s', the page is not ready",
+                        this.eta,
+                        response.xpath("/page/identity/name/text()").get(0)
+                    )
                 );
-                throw new AssertionError();
             }
         } else if (response.getStatus() == HttpURLConnection.HTTP_UNAVAILABLE) {
             this.eta = 1L;
-            Logger.warn(
-                this,
-                "assertThat(..): service is temporary unavailable"
-            );
-            throw new AssertionError();
+            throw new AssertionError("service is temporary unavailable");
         }
     }
 
