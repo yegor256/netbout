294f41e3cdb80b5bf53b3653b361e4b7dec4049e
Yegor Bugayenko <yegor@tpc2.com>
2014-04-27 18:57:34 +0200

    #master: build is clean

--- netbout-web/src/test/casperjs/inbox.js
+++ netbout-web/src/test/casperjs/inbox.js
@@ -1,5 +1,6 @@
+/*globals require:false, test:false */
 var casper = require('casper').create();
-var home = casper.cli.get "home";
+var home = casper.cli.get("home");
 casper.start(
     home + '/xml/inbox.xml',
     function() {
