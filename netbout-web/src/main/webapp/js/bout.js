6ae3095ce86192cea99427e87ec73b0295443ea7
Yegor Bugayenko <yegor@tpc2.com>
2014-06-23 22:40:47 +0200

    fetch in 400 pixels

--- netbout-web/src/main/webapp/js/bout.js
+++ netbout-web/src/main/webapp/js/bout.js
@@ -57,7 +57,7 @@ $(document).ready(
     $(window).scroll(
       function () {
         var $box = $('#messages'), $tail = $('#tail'), more = $box.attr('data-more');
-        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 50 && more) {
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 400 && more) {
           $box.removeAttr('data-more', '');
           $tail.show();
           $.ajax(
--- netbout-web/src/main/webapp/js/inbox.js
+++ netbout-web/src/main/webapp/js/inbox.js
@@ -40,7 +40,7 @@ $(document).ready(
     $(window).scroll(
       function () {
         var $box = $('#bouts'), $tail = $('#tail'), more = $box.attr('data-more');
-        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 50 && more) {
+        if ($(window).scrollTop() >= $(document).height() - $(window).height() - 400 && more) {
           $box.removeAttr('data-more', '');
           $tail.show();
           $.ajax(
