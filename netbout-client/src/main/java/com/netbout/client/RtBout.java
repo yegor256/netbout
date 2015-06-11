66604959ab160d20cc85bc6c112e2a0c5cc33699
Eugene Kondrashev <eugene.kondrashev@gmail.com>
2015-06-11 21:39:14 +0300

    Added space requested by pdd

--- netbout-client/src/main/java/com/netbout/client/RtBout.java
+++ netbout-client/src/main/java/com/netbout/client/RtBout.java
@@ -148,8 +148,8 @@ final class RtBout implements Bout {
     }
 
     // @todo #682:30min/DEV Implement getting subscription by alias.
-    // Should return subscription status for current boat,
-    // passing friend alias value using REST call.
+    //  Should return subscription status for current boat,
+    //  passing friend alias value using REST call.
     @Override
     public boolean subscription(final String alias) throws IOException {
         throw new UnsupportedOperationException("#subscription(alias)");
