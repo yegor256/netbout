a122ad8ce103489b991097f5783807767518bf86
Mihai Emil Andronache <amihaiemil@gmail.com>
2016-08-10 11:03:52 +0300

    fixed type "test" -> "search"

--- netbout-web/src/test/casperjs/inbox-noresults-message.js
+++ netbout-web/src/test/casperjs/inbox-noresults-message.js
@@ -1,6 +1,6 @@
 /*globals casper:false */
 casper.test.begin(
-    'message is displayed if there are no test results',
+    'message is displayed if there are no search results',
     function (test) {
         casper.start().then(
             function() {
